bbf6f7480d364450dcc4faf8b0449ed78ec033fa
HadrienDESCHET <hadrien.deschet@viacesi.fr>
2017-06-21 10:36:14 +0200

    Management of controls

--- BoulderDash/view/src/main/java/view/EventPerformer.java
+++ BoulderDash/view/src/main/java/view/EventPerformer.java
@@ -15,9 +15,33 @@ public class EventPerformer implements IEventperformer{
 	
 	public void eventPerform(KeyEvent keyCode){
 		
+		final UserOrder userOrder = this.keyCodeToUserOrder(keyCode.getKeyCode());
+		if (userOrder != null) {
+			this.orderPerformer.orderPerform(userOrder);
+		}
 	}
 	
 	private UserOrder keyCodeToUserOrder(int keyCode){
 		
+		switch (keycode) {
+		case KeyEvent.VK_UP:
+			userOrder = new UserOrder(Order.UP);
+			break;
+		case KeyEvent.VK_RIGHT:
+			userOrder = new UserOrder(Order.RIGHT);
+			break;
+		case KeyEvent.VK_DOWN:
+			userOrder = new UserOrder(Order.DOWN);
+			break;
+		case KeyEvent.VK_LEFT:
+			userOrder = new UserOrder(Order.LEFT);
+			break;
+		case KeyEvent.VK_CONTROL:
+			userOrder = new UserOrder(Order.PUSH);
+			break;
+		default:
+			userOrder;
+		}
+		return userOrder;
 	}
 }
