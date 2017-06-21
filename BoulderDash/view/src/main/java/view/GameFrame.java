4f15c8a78e227208e85982934122a38f984e9f73
HadrienDESCHET <hadrien.deschet@viacesi.fr>
2017-06-21 09:19:37 +0200

    Adding GameFrame to View

---
+++ BoulderDash/view/src/main/java/view/GameFrame.java
@@ -0,0 +1,12 @@
+package view;
+
+public class GameFrame {
+
+		public GameFrame(String title, IEventPerformer performer, IGraphicsBuilder graphicBuilder, Observable observable){
+			
+		}
+		
+		public void keyPressed( KeyEvent keypressed){
+			
+		}
+}
