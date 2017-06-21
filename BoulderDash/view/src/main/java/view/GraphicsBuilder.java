5323a039521b690c45a9103df725f28f3e7d524a
HadrienDESCHET <hadrien.deschet@viacesi.fr>
2017-06-21 10:05:46 +0200

    Adding GamePanel, GraphicsBuilder and IGraphicsBuilder modified

---
+++ BoulderDash/view/src/main/java/view/GraphicsBuilder.java
@@ -0,0 +1,31 @@
+package view;
+
+import java.awt.Graphics;
+import java.awt.image.ImageObserver;
+
+import model.IMobile;
+import model.IModelFacade;
+
+public class GraphicsBuilder {
+
+public GraphicsBuilder(IModelFacade Model){
+		
+	}
+	public void applyModelToGraphic(Graphics graphics, ImageObserver observer){
+		
+	}
+	 private void buildEmptyGround(){
+		 
+	 }
+	 private void drawMobile(IMobile mobile, Graphics graphics, ImageObserver observer){
+		 
+	 }
+	 public int getGlobalWidth(){
+		return 0;
+		 
+	 }
+	 public int getGlobalHeight(){
+		return 0;
+		 
+	 }
+}
